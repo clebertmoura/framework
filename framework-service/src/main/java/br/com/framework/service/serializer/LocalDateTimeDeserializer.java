7652f1aae5d8b4338cb53199a0804ecac0236e53
clebertmoura <cleber.t.moura@gmail.com>
2019-04-22 08:14:53 -0300

    ajustes

--- framework-service/src/main/java/br/com/framework/service/serializer/LocalDateTimeDeserializer.java
+++ framework-service/src/main/java/br/com/framework/service/serializer/LocalDateTimeDeserializer.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,6 +23,12 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(LocalDateTimeDeserializer.class);
+	
+	public static final DateTimeFormatter[] DATETIME_FORMATS = {
+			DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+	};
 
 	private static final DateTimeFormatter JAVASCRIPT_DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
@@ -32,11 +42,20 @@ public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
 
 	@Override
 	public LocalDateTime deserialize(JsonParser json, DeserializationContext ctx) throws IOException, JsonProcessingException {
-		return LocalDateTime
-					.parse(json.getValueAsString(), JAVASCRIPT_DATETIME_FORMAT)
-					.atZone(UTC_TIMEZONE)
-					.withZoneSameInstant(RECIFE_TIMEZONE)
-					.toLocalDateTime();
+		LocalDateTime localDateTime = null;
+		for (DateTimeFormatter dateTimeFormatter : DATETIME_FORMATS) {
+			try {
+				localDateTime = LocalDateTime
+						.parse(json.getValueAsString(), dateTimeFormatter)
+						.atZone(UTC_TIMEZONE)
+						.withZoneSameInstant(RECIFE_TIMEZONE)
+						.toLocalDateTime();
+				break;
+			} catch (DateTimeParseException e) {
+				LOG.error(String.format("Erro ao deserializar: %s, formato: %s", json.getValueAsString(), dateTimeFormatter), e);
+			}
+		}
+		return localDateTime;
 	}
 
 
